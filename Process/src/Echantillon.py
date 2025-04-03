a01f8f02365bbb0ae3090a3a6ad156602af92a6e
Xiaobo <sibelwang17@gmail.com>
2025-04-03 11:14:49 +0200

    Ajout des scripts

---
+++ Process/src/Echantillon.py
@@ -0,0 +1,14 @@
+import json
+import random
+
+input_file = "Books.jsonl"
+output_file = "Books_échantillon.jsonl"
+sample_size = 10000  # je garde 10000 lignes au hasard dans le corpus
+
+with open(input_file, "r", encoding="utf-8") as infile:
+    lines = infile.readlines()
+
+échantillon = random.sample(lines, min(sample_size, len(lines)))
+
+with open(output_file, "w", encoding="utf-8") as outfile:
+    outfile.writelines(échantillon)
---
+++ Process/src/split_dataset.py
@@ -0,0 +1,24 @@
+import json
+from sklearn.model_selection import train_test_split
+
+data = []
+with open("Books.jsonl", "r", encoding="utf-8") as file:
+    for line in file:
+        review = json.loads(line)
+        data.append(review)
+
+# on divise les 3 datasets en 80%-10%-10%
+train, temp = train_test_split(data, test_size=0.2, random_state=42)
+dev, test = train_test_split(temp, test_size=0.5, random_state=42)
+
+# sauvegarde des datasets
+def save_jsonl(filename, dataset):
+    with open(filename, "w", encoding="utf-8") as file:
+        for entry in dataset:
+            file.write(json.dumps(entry) + "\n")
+
+save_jsonl("Books_train.jsonl", train)
+save_jsonl("Books_dev.jsonl", dev)
+save_jsonl("Book_test.jsonl", test)
+
+print(f"C'est fait ：train {len(train)}，dev : {len(dev)}，test {len(test)}")
