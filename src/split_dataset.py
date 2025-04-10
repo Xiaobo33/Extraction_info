5c976382ff5002dcc434f618330f0e39d97b33ac
simeng-song <song_simeng2024@163.com>
2025-04-10 11:58:44 +0200

    modification du script - les datasets csv train/dev/test

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -1,24 +1,15 @@
-import json
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
-data = []
-with open("Books_cleaned.jsonl", "r", encoding="utf-8") as file:
-    for line in file:
-        review = json.loads(line)
-        data.append(review)
+df = pd.read_csv("Kindle_2_extracted.csv")
 
 # on divise les 3 datasets en 80%-10%-10%
-train, temp = train_test_split(data, test_size=0.2, random_state=42)
-dev, test = train_test_split(temp, test_size=0.5, random_state=42)
+train, temp = train_test_split(df, test_size=0.2, stratify=df['label'], random_state=42)
+dev, test = train_test_split(temp, test_size=0.5, stratify=temp['label'], random_state=42)
 
 # sauvegarde des datasets
-def save_jsonl(filename, dataset):
-    with open(filename, "w", encoding="utf-8") as file:
-        for entry in dataset:
-            file.write(json.dumps(entry) + "\n")
-
-save_jsonl("Books_train.jsonl", train)
-save_jsonl("Books_dev.jsonl", dev)
-save_jsonl("Books_test.jsonl", test)
+train.to_csv("Kindle_2_train.csv", index=False)
+dev.to_csv("Kindle_2_dev.csv", index=False)
+test.to_csv("Kindle_2_test.csv", index=False)
 
 print(f"Mission complete ：train {len(train)}，dev : {len(dev)}，test {len(test)}")
